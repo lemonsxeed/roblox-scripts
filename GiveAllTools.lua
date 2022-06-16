140ca8750d96cb956e806b5141d0100a2f08bd62
lemonsxeed <84166176+lemonsxeed@users.noreply.github.com>
2022-06-16 20:22:28 +0700

    Update GiveAllTools.lua

--- GiveAllTools.lua
+++ GiveAllTools.lua
@@ -9,6 +9,9 @@ workspace.ChildAdded:Connect(function(child)
     if child:IsA("Tool") then
         local handle = child:WaitForChild("Handle")
         local height = (0.5 * RootPart.Size.Y) + game.Players.LocalPlayer.Character.Humanoid.HipHeight
+        local savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
         game.Players.LocalPlayer.Character.HumanoidRootPart.Position = Vector3.new(handle.Position.X, handle.Position.Y + height, handle.Position.Z)
+        repeat wait(0.3) until tool.Parent = game.Players.LocalPlayer.Character or tool.Parent = game.Players.LocalPlayer.Backpack
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Position = savepos
     end
 end)
